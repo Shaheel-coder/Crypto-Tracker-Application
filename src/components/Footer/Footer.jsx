e3c8fc5ac304a798cfefbca3003c93d793f81e69
Shaheel-coder <mohamedshaheel56@gmail.com>
2025-04-18 13:41:59 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <a >Copyright @ 2024,Shaheel -All Rights Reserved</a>
+    </div>
+  )
+}
+
+export default Footer
